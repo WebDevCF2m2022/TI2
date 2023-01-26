ae289753c8d20f6784b4179b0c5c211e9fa888d9
JonRauzy <jonathan.rauzy@gmail.com>
2023-01-26 12:11:31 +0100

    v0.1

--- public/index.php
+++ public/index.php
@@ -1,3 +1,70 @@
 <?php
 
+// LO = livre d'or
+
 require_once "../config.php";
+
+// connection à la DB avec try catch
+try{
+    $dbLO = mysqli_connect(DB_HOST, DB_LOGIN, DB_PWD, DB_NAME, DB_PORT);
+    mysqli_set_charset($dbLO, DB_CHARSET);
+    
+}catch(Exception $e){
+    
+    exit(mb_convert_encoding($e->getMessage(), 'UTF-8', 'ISO-8859-1'));
+    
+}
+
+// insertion d'articles dans la DB par l'utilisateur :
+if (isset($_POST['firstname'], $_POST['lastname'], $_POST['usermail'], $_POST['message'])) {
+
+    $firstname = htmlspecialchars(strip_tags(trim($_POST['firstname'])));
+    $lastname = htmlspecialchars(strip_tags(trim($_POST['lastname'])));
+    $usermail = htmlspecialchars(strip_tags(trim($_POST['usermail'])));
+    $message = htmlspecialchars(strip_tags(trim($_POST['message'])), ENT_QUOTES);
+
+
+    # si les champs sont bons (ici vide, donc une seule erreur générale)
+    if (!empty($firstname) && !empty($lastname) && filter_var($usermail,FILTER_VALIDATE_EMAIL) &&!empty($message)) {
+
+        # insertion partie SQL
+        $sqlInsert = "INSERT INTO `livreor` (`firstname`,`lastname`, `usermail`, `message`) VALUES ('$firstname','$lastname', '$usermail', '$message');";
+
+        try{
+
+            mysqli_query($dbLO, $sqlInsert);
+            $messageRetour = "t'es bien inscrit copain!";
+
+        }catch(Exception $e){
+
+            if($e->getCode()==1406){
+                # création de l'erreur
+                $message = "Un champs est trop long";
+
+            }elseif($e->getCode()==1062){
+                # création de l'erreur
+                $message = "Vous êtes déjà inscrit avec ce mail";
+            }
+        }
+    }else{
+         if(!filter_var($usermail,FILTER_VALIDATE_EMAIL)){
+            $messageRetour = "mail pas valide";
+        }else{
+            $messageRetour = "il y a un problème! rééssayez!";
+        }
+    }
+}
+
+// recupération des messages qui sont dans la DB (nom prénom messages et date) : 
+$sqlLO = "SELECT `firstname`, `lastname`, `message`, `datemessage` FROM `livreor` ORDER BY `datemessage` DESC;";
+$queryLO = mysqli_query($dbLO, $sqlLO);
+$nbLO = mysqli_num_rows($queryLO);
+$resultLO = mysqli_fetch_all($queryLO, MYSQLI_ASSOC);
+
+
+// on ferme la DB derrière nous parce qu'on est poli :
+mysqli_free_result($queryLO);
+mysqli_close($dbLO);
+
+// affichage de la vue :
+include_once '../view/indexView.php';
--- view/indexView.php
+++ view/indexView.php
@@ -1,1 +1,59 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Livre D'or de Jon</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <!-- titre -->
+    <h1>Travail</h1>
+    <!-- formulaire utilisateur -->
+    <form action="" method="post">
+        <input type="text" name="firstname" id="firstname" placeholder="prénom">
+        <input type="text" name="lastname" id="lastname" placeholder="nom de famille">
+        <input type="mail" name="usermail" id="usermail" placeholder="email">
+        <textarea name="message" id="message" cols="30" rows="10" placeholder="votre texte"></textarea>
+
+        <!-- mettre en type="button" hein -->
+        <button type="submit">envoyer</button>
+    </form>
+    <?php if (isset($messageRetour)) : ?>
+        <?= $messageRetour ?>
+    <?php
+    endif;
+    ?>
+    <hr>
+    <div style="padding: 10rem;"></div>
+
+
+
+    <div class="message-affiche">
+        <?php
+        // var_dump($resultLO);
+
+        // on vérifie si y'a des messages dans le LO et on affiche combien y'en a si y'en a :
+
+        if (empty($nbLO)) : ?>
+            <?= "il y a pas de messages ici"; ?>
+        <?php else : ?>
+            <?= "il y a " . $nbLO . " adresses"; ?>
+        <?php endif; ?>
+
+        <!-- affichage des éléments des la DB -->
+        
+        <?php foreach ($resultLO as $item) : ?>
+            <p><?= $item['firstname']; ?></p>
+            <p><?= $item['lastname']; ?></p>
+            <p><?= $item['message']; ?></p>
+            <p><?= $item['datemessage']; ?></p>
+        <?php endforeach; ?>
+    </div>
+
+</body>
+
+</html>
