517b9cf8a0956b0d55c246df59df489acd31f17b
flow3flow <floramine75@hotmail.com>
2023-01-26 16:12:00 +0100

    ti

--- public/index.php
+++ public/index.php
@@ -1,3 +1,96 @@
 <?php
-
+#Chargement des paramètres de connexions
 require_once "../config.php";
+
+# Essai de connexion
+try{
+    # connexion mysqli
+    $db = mysqli_connect(DB_HOST,DB_LOGIN,DB_PWD,DB_NAME,DB_PORT);
+    # charset
+    mysqli_set_charset($db,DB_CHARSET);
+
+# capture l'erreur
+}catch(Exception $e){
+
+    # arrêter le script et afficher l'erreur
+    exit(utf8_encode($e->getMessage()));
+
+}
+# si il existe les variables POST = formulaire envoyé
+if(isset($_POST['firstname'])&& isset($_POST['usermail'])&& isset( $_POST['message'] )){
+    # traitement des champs contre injection SQL (Sécurité!)
+    $prenom = htmlspecialchars(strip_tags(trim($_POST['firstname'])),ENT_QUOTES);
+    $nom=htmlspecialchars(strip_tags(trim($_POST['lastname'])),ENT_QUOTES);
+    $mail = htmlspecialchars(strip_tags(trim($_POST['usermail'])),ENT_QUOTES); // on pourrait vérifier si c'est un mail valide ( filter_var voir la fonction sur php.net)
+    $messageUser = htmlspecialchars(strip_tags(trim($_POST['message'])),ENT_QUOTES);
+
+    # si les champs sont bons (ici vide, donc une seule erreur générale)
+    if(!empty($prenom)&&!empty($mail)&&!empty($messageUser)){
+        
+        # insertion partie SQL
+       $sqlInsert = "INSERT INTO `livreor` (`firstname`, `lastname`, `usermail`, `message`) VALUES ('$prenom', '$nom', '$mail', '$messageUser')";
+        # requête avec try catch
+     
+        var_dump(($sqlInsert));
+        try{
+            # requête
+      $result=mysqli_query($db,$sqlInsert);
+      header("Location: ./");
+            # si pas d'erreur création du texte
+            $info ="Merci pour votre inscription";
+
+        }catch(Exception $e){
+           # echo $e->getCode()
+
+           # avec le code erreur SQL on peut faire des erreurs différentes, idem avec le $e->getMessage() etc...
+            if($e->getCode()==1406){
+                # création de l'erreur
+                $info = "Un champs est trop long";
+
+            }elseif($e->getCode()==1062){
+                # création de l'erreur
+                $info = "Vous êtes déjà inscrit avec ce mail";
+            }
+            
+        }
+
+
+    # sinon erreur
+    }else{
+        # création de la variable $message
+        $info = "Il y a eu un problème lors de votre inscription, veuillez réessayer";
+    }
+
+}
+
+# chargement de tous les mails
+
+// requête en variable texte contenant du MySQL
+$sqlMail = "SELECT * FROM `livreor` ORDER BY `datemessage` DESC; ";
+
+// exécution de la requête avec un try / catch
+try {
+    $queryMail = mysqli_query($db, $sqlMail);
+}catch(Exception $e){
+    # arrêter le script et afficher l'erreur (de type SQL)
+    exit(utf8_encode($e->getMessage()));
+}
+
+
+# on compte le nombre de mails récupérés
+$nbMail = mysqli_num_rows($queryMail);
+
+# on convertit les mails récupérés en tableaux associatifs intégrés dans un tableau indexé
+$responseMail = mysqli_fetch_all($queryMail,MYSQLI_ASSOC);
+
+
+
+# on efface les données récupérées pas un SELECT (bonnes pratiques)
+mysqli_free_result($queryMail);
+# fermeture de connexion  (bonnes pratiques)
+mysqli_close($db); 
+
+# appel de la vue
+include_once '../view/indexView.php';
+
+
--- view/indexView.php
+++ view/indexView.php
@@ -1,1 +1,108 @@
 <?php
+var_dump($_POST); 
+?>
+<?php
+# débugogage de la variable POST
+//var_dump($_POST);   
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/captcha.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <script src="js/captcha.js" defer></script>
+    <title>Mail</title>
+</head>
+<body>
+    <h1>Mail</h1>
+    <h2>Formulaire</h2>
+    <?php
+    # si on a un message
+    if(isset($message)):
+        # on l'affiche
+    ?>
+    <h4><?=$message?></h4>
+    <?php
+    endif;
+    ?>
+    <!--<form name='lemail' action='' method="POST">
+            <input type="text" name="nomadresses" placeholder="votre nom" required><br>
+            <input type='email' name="mailadresses" placeholder="votre mail" required><br>
+            <input type="submit" value="Envoyer">
+        </form>-->
+           <form name='monFormulaire' action='' method="POST"> 
+        <div class="loginFormulaire" id="monFormulaire">
+      <div class="formTitle">
+        <h1>Member Login</h1>
+      </div>
+      <div class="form-input">
+        <label for="username">Prénom *</label>
+        <input type="text" name="firstname" placeholder="Saisissez votre prénom"  id="username" required />
+      </div>
+      <div class="form-input">
+        <label for="username">Nom</label>
+        <input type="text" name="lastname" placeholder="Saisissez votre nom"  id="username" />
+      </div>
+      <div class="form-input">
+        <label for="email">E-mail *</label>
+        <input type="email" name="usermail" placeholder="mail"  id="Saisissez votre email" required />
+      </div>
+      <div class="form-input">
+        <label for="Message">Message *</label>
+        <textarea name="message" placeholder="message"  id="message" required /></textarea>
+      </div>
+      <div class="captchaValidate">
+            <button id="captchaValidate">Valider</button>
+            <!--<button id="captchaRefresh">Refresh</button> -->
+          </div>
+
+      
+      <div class="captcha">
+        <label for="captcha">Entrer Captcha</label>
+        <div class="captc" id="captcha"></div>
+
+        <div class="captcha-form">
+          <input
+            type="texte"
+            id="captchaInput"
+            placeholder="Entrer captcha text"
+          />
+          <div class="captchaValidate">
+            <!--<button id="captchaValidate">Login</button>-->
+           <button id="captchaRefresh">Refresh</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  
+    <h3>Les mails</h3>
+    <?php
+    # pas de mail
+    if(empty($nbMail)):
+    ?>
+    <h4>Nous avons <?=$nbMail?> adresses inscrites</h4>
+    <?php
+    # on a au moins un mail
+    else:
+        # affichage du nombre de mail
+     
+        # tant qu'on a des mail
+        foreach($responseMail as $item):
+        ?>
+<div class='theMail'>
+    <?=$item['firstname']?>
+    <?=$item['lastname']?>
+    <?=$item['usermail']?>
+    <?=$item['datemessage']?>
+    <br>
+    <?=$item['message']?>
+  </div>
+        <?php
+        endforeach;
+    endif;
+    ?>
+</body>
+</html>
