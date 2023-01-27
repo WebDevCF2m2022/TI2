1a5e5f2d4dfefcc8b167ec4c3685c62a528e1d1d
JonRauzy <jonathan.rauzy@gmail.com>
2023-01-27 14:57:03 +0100

    v1.0

--- view/indexView.php
+++ view/indexView.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Livre D'or de Jon</title>
     <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/captcha">
+    <link rel="stylesheet" href="./css/captcha">
 </head>
 
 <body>
